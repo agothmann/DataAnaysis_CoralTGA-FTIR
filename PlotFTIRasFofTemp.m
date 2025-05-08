63debeac7b707c294ef840242e18375c73e0a29c
elizabethce20 <ernst1@stolaf.edu>
2025-05-08 18:35:03 -0500

    Updated Code (5/8/2025)
    Updated to subtract strange background from water and CO2. Must use this version of the function backcor to run the program correctly (backcor.m must be in the same folder as PlotFTIRasFofTemp.m)

--- PlotFTIRasFofTemp.m
+++ PlotFTIRasFofTemp.m
@@ -1,8 +1,11 @@
 % Script to read in TGA FTIR data and plot absorption at a wavenumber as a
 % function of temp. 
 
-%None of the code in this script will inverse any data, nor will it
-%subtract the initial run background from the data
+% None of the code in this script will inverse any data, nor will it
+% subtract the initial run background from the data
+
+% To run this script, make sure our version of the backcor function is in
+% the same folder as this script
 
 
 %This code promts us to select the first data sample
@@ -29,27 +32,34 @@ for i = 1:(length(TEXTfiles))
     end
 end
 
+% configuring the filename just for data acquision and graph title purposes
 filename = split(filename, '_Filenames.xlsx');
 
 
 %ONE TIME read in of excel sheet, finding the right sample name
+% This is also used for sample A, but is not re-imported
 SpaData = readcell('C:\Users\lizzy_u4nmadb\OneDrive\Documents\MATLAB\TGAFTIR_DATA\SPA_Time_Data.xlsx');
 sizeSpa = size(SpaData);
 rowSpa = sizeSpa(1,1);
 
+%This is using the input sample name to find the data for the right sample
 for i = 1:1:rowSpa
     str1 = string(SpaData{i,1});
     str2 = string(filename{1:1});
     if strcmp(str1,str2)
+        %as it runs through the rows of SpaData, if the string it sees in
+        %the row is the same as the string of the filename we input
         DataRow = i;
+        %then take note of that row, and assign it to DataRow
     end
 end
 
-%reading in data from the above Excel sheet
+%reading in the correct data from the above Excel sheet
 RunStart = SpaData{DataRow,2}; %sample run START time
 RunEnd = SpaData{DataRow,3}; %sample run END time
 RunDivision = SpaData{DataRow,4}; %number of SPA files for this particular dataset
 subtract = SpaData{DataRow,5}; %data time correction, if needed
+back_corr = SpaData{DataRow,6}; % 0 or 1 on whether the sample needs background subtraction
 
 STEP = ((RunEnd-RunStart)/(RunDivision-1));
 FTIR_time = RunStart:STEP:RunEnd;
@@ -62,10 +72,10 @@ filename = char(strrep(filename,'_','-'));
 WaterData = zeros((length(TEXTfiles)),1);
 CO2Data = zeros((length(TEXTfiles)),1);
 
-
+% selecting the specific wavenumber for Water and CO2
 for jj = 1:(length(TEXTfiles))
-    WaterData(jj,:) = MasterData(3768,2,jj); %*(-1);
-    CO2Data(jj,:) = MasterData(7256,2,jj); %*(-1);
+    WaterData(jj,:) = MasterData(3768,2,jj);
+    CO2Data(jj,:) = MasterData(7256,2,jj);
 end
 
 
@@ -79,6 +89,8 @@ for r = 1:length(TGA_data(:,1))
     str3 = string(TGA_data(r,1));
     chop = "StartOfData";
     if strcmp(str3,chop) == true
+        %when iterating through all the rows of the text file, when the row
+        %reads "StartOfData", assign that row index to ChopAfter
         ChopAfter = r;
     end
 end
@@ -98,10 +110,43 @@ Col5	Sample Purge Flow (mL/min)
 
 TGA_time = TGA_data(:,1);
 TGA_time = cell2mat(TGA_time)-subtract; %subtracting warm up time period, if needed
-TGA_time = (TGA_time.');
+TGA_time = (TGA_time.'); 
 
 TGA_temp = cell2mat(TGA_data(:,2));
-weight = cell2mat(TGA_data(:,3));
+weight = cell2mat(TGA_data(:,3)); %not currently used
+
+
+
+% NEW ADDITION FOR BACKGROUND SUBTRACTION
+
+% Cut FTIR_time off when it starts getting bigger than the end value of
+% TGA_time
+
+%find the last value of TGA_time
+lastTGA = TGA_time(1,end);
+cutData = zeros(length(FTIR_time),1);
+
+index = 1;
+for val = 1:length(FTIR_time)
+    %iterate through the indices of FTIR_time
+    if lastTGA < FTIR_time(1,val)
+        %if the highest value of the TGA data is less than a data value in
+        %FTIR time, then take note of the index
+        cutData(index) = val;
+        %taking note of each index where above is true, and appending it to
+        %a list, but we only will need the value in the FIRST index of this
+        index = index + 1;
+    end
+end
+
+FTIR_time = FTIR_time(1,1:(cutData(1,1))-1);
+%end FTIR time before the values start getting larger than the existing TGA
+%data
+
+% END NEW ADDITION
+
+
+
 
 %INTERPOLATE instead of curve fit
 FTIR_temp = interp1(TGA_time,TGA_temp,FTIR_time);
@@ -136,7 +181,7 @@ while pp <= (length(FTIR_time))-1
 end
 
 
-%%
+
 
 %IMPORTING A SECOND RUN, labeled A
 
@@ -176,6 +221,7 @@ RunStart_A = SpaData{DataRow_A,2};
 RunEnd_A = SpaData{DataRow_A,3}; 
 RunDivision_A = SpaData{DataRow_A,4};
 subtract_A = SpaData{DataRow_A,5};
+back_corr_A = SpaData{DataRow_A,6};
 
 STEP_A = ((RunEnd_A-RunStart_A)/(RunDivision_A-1));
 FTIR_time_A = RunStart_A:STEP_A:RunEnd_A;
@@ -188,8 +234,8 @@ CO2DataA = zeros((length(TEXTfilesA)),1);
 
 
 for jj = 1:(length(TEXTfilesA))
-    WaterDataA(jj,:) = MasterDataA(3768,2,jj); %*(-1);
-    CO2DataA(jj,:) = MasterDataA(7256,2,jj); %*(-1);
+    WaterDataA(jj,:) = MasterDataA(3768,2,jj);
+    CO2DataA(jj,:) = MasterDataA(7256,2,jj);
 end
 
 
@@ -217,6 +263,37 @@ TGA_time_A = (TGA_time_A.'); %convert time to a row instead of column
 
 TGA_temp_A = cell2mat(TGA_data_A(:,2));
 weight_A = cell2mat(TGA_data_A(:,3));
+
+
+
+
+
+
+% NEW ADDITION FOR BACKGROUND SUBTRACTION
+
+% Cut FTIR_time off when it starts getting bigger than the end value of
+% TGA_time
+
+%find the last value of TGA_time
+lastTGA_A = TGA_time_A(1,end);
+cutData_A = zeros(length(FTIR_time_A),1);
+
+index_A = 1;
+for val_A = 1:length(FTIR_time_A) %comments in section above
+    if lastTGA_A < FTIR_time_A(1,val_A)
+        
+        cutData_A(index_A) = val_A;
+        index_A = index_A + 1;
+    end
+end
+
+FTIR_time_A = FTIR_time_A(1,1:(cutData_A(1,1))-1);
+
+%END NEW ADDITION
+
+
+
+
 
 %INTERPOLATE instead of curve fit
 FTIR_temp_A = interp1(TGA_time_A,TGA_temp_A,FTIR_time_A);
@@ -253,13 +330,203 @@ end
 
 
 
-%%
+%% BACKGROUND SUBTRACTION SET UP
 
-figure(1) %comparing the water peaks of the two samples
-plot(FTIR_temp,WaterData, "LineWidth",1,"Color","b")
+%Need to change the formatting of these arrays so backcor will take them
+FTIR_temp = FTIR_temp.';
+WaterData = WaterData.';
+CO2Data = CO2Data.';
+
+FTIR_temp_A = FTIR_temp_A.';
+WaterDataA = WaterDataA.';
+CO2DataA = CO2DataA.';
+
+%%
+% cut the CO2 data for every datapoint before the temp 50 to get rid of
+% outlying data points at the beginning of the run
+CO2_cut = 50;
+new_CO2_start = 0;
+o = 1;
+while new_CO2_start == 0
+    %while nothing has changed with the initialized variable
+    if floor(FTIR_temp(o,1)) > CO2_cut
+        %if the rounded down value in the 'oth' row of FTIR temp data is
+        %BIGGER than 50
+        new_CO2_start = o;
+        %set the initialized variable to the first index where that is true
+    end
+    o = o + 1;
+    %it continues interating only if new_CO2_start is unchanged, as in, 0
+end
+%need to cut BOTH the CO2Data and the FTIR temp data in order to use the
+%backcor function and graph the data correctly
+CO2Data = CO2Data(1, new_CO2_start:end);
+FTIR_temp_CO2 = FTIR_temp(new_CO2_start:end, 1);
+
+CO2_cut = 50;
+new_CO2_start = 0;
+o = 1;
+while new_CO2_start == 0
+    if floor(FTIR_temp_A(o,1)) > CO2_cut
+        new_CO2_start = o;
+    end
+    o = o + 1;
+end
+CO2DataA = CO2DataA(1, new_CO2_start:end);
+FTIR_temp_CO2_A = FTIR_temp_A(new_CO2_start:end, 1);
+
+%cutting the H2o at temp 40
+%everything is the same for water, except the data is cut off at 40 degrees
+H2O_cut = 40;
+new_H2O_start = 0;
+o = 1;
+while new_H2O_start == 0
+    if floor(FTIR_temp(o,1)) > H2O_cut
+        new_H2O_start = o;
+    end
+    o = o + 1;
+end
+WaterData = WaterData(1, new_H2O_start:end);
+FTIR_temp_H2O = FTIR_temp(new_H2O_start:end, 1);
+
+H2O_cut = 40;
+new_H2O_start = 0;
+o = 1;
+while new_H2O_start == 0
+    if floor(FTIR_temp_A(o,1)) > H2O_cut
+        new_H2O_start = o;
+    end
+    o = o + 1;
+end
+WaterDataA = WaterDataA(1, new_H2O_start:end);
+FTIR_temp_H2O_A = FTIR_temp_A(new_H2O_start:end, 1);
+
+%% BACKGROUND CORRECTION FUNCTION
+
+ord = 4; %Good order for water has been 4
+CO2ord = 2; %Good order for CO2 has been 2
+
+% If the sample selected doesn't need background correction, the program
+% will figure that out from the Spa Data excel, and it will set the
+% variable back_corr to 0, so that this next loop will set the ord to 1
+if back_corr == 0
+    ord = 1;
+    CO2ord = 1;
+end
+
+% s is the threshold number (how low of number will be considered positive)
+s = 0.01;
+% fct is the type of function used to estimate the background
+% so far the fct has not seemed to matter
+fct = 'atq';
+
+%THIS FUNCTION HAS NO POP UP
+[z,a,it,ord,s,fct] = backcor(FTIR_temp_H2O, WaterData, ord, s, fct);
+[zz, aa, itit, ordord, ss, fctfct] = backcor(FTIR_temp_CO2, CO2Data, CO2ord, s, fct);
+
+
+ord_A = 4;
+CO2ord_A = 2;
+
+if back_corr_A == 0
+    ord_A = 1;
+    CO2ord_A = 1;
+end
+
+%THIS FUNCTION HAS NO POP UP
+[z_A,a_A,it_A,ord_A,s_A,fct_A] = backcor(FTIR_temp_H2O_A, WaterDataA, ord_A, s, fct);
+[zz_A, aa_A, itit_A, ordord_A, ss_A, fctfct_A] = backcor(FTIR_temp_CO2_A, CO2DataA, CO2ord_A, s, fct);
+
+
+%{
+%THIS FUNCTION HAS A POP UP ASKING YOU TO VERIFY THE BACKGROUND ORDER
+%AND IT WILL SET THE THRESHOLD AND FUNC FOR YOU
+
+[z, a, it, ord, s, fct] = backcor(FTIR_temp_H2O, WaterData);
+[zz, aa, itit, ordord, ss, fctfct] = backcor(FTIR_temp_CO2, CO2Data);
+
+[z_A, a_A, it_A, ord_A, s_A, fct_A] = backcor(FTIR_temp_H2O_A,WaterDataA);
+[zz_A, aa_A, itit_A, ordord_A, ss_A, fctfct_A] = backcor(FTIR_temp_CO2_A, CO2DataA);
+%}
+
+
+%Sorting and formatting more data in order to plot correctly
+% !! taken from demo.m given with the backcor function !!
+if ~isempty(z)
+    
+    [FTIR_temp_H2O,i] = sort(FTIR_temp_H2O);
+    WaterData = WaterData(i);
+    z = z(i);
+    z = z.';
+    
+end
+
+if ~isempty(zz)
+    
+    [FTIR_temp_CO2,i] = sort(FTIR_temp_CO2);
+    CO2Data = CO2Data(i);
+    zz = zz(i);
+    zz = zz.';
+    
+end
+
+if ~isempty(z_A)
+    
+    [FTIR_temp_H2O_A,i] = sort(FTIR_temp_H2O_A);
+    WaterDataA = WaterDataA(i);
+    z_A = z_A(i);
+    z_A = z_A.';
+    
+end
+
+if ~isempty(zz_A)
+    
+    [FTIR_temp_CO2_A,i] = sort(FTIR_temp_CO2_A);
+    CO2DataA = CO2DataA(i);
+    zz_A = zz_A(i);
+    zz_A = zz_A.';
+    
+end
+
+
+%% PLOTTING
+
+%{
+% Combined sample plots of water and CO2 data
+figure(1) 
+%Water
+subplot(1,2,1)
+plot(FTIR_temp_H2O,WaterData-z, "LineWidth",1,"Color","b")
+%z is the background that is being subtracted from WaterData
 hold on
-plot(FTIR_temp_A,WaterDataA, "LineWidth",1,"Color","c")
-title("H2O Data - absorption at peak wavenumber")
+plot(FTIR_temp_H2O_A,WaterDataA-z_A, "LineWidth",1,"Color","c")
+title('Both samples')
+subtitle("H2O Data (BKG CORR)")
+xlabel("Temp (C)")
+ylabel("H2O Absorption")
+legend(filename, filenameA, "Location","northwest")
+grid on
+hold off
+%Co2
+subplot(1,2,2)
+plot(FTIR_temp_CO2,CO2Data-zz, "LineWidth",1,"Color","#7E2F8E")
+hold on
+plot(FTIR_temp_CO2_A,CO2DataA-zz_A, "LineWidth",1,"Color","#FF00FF")
+subtitle("CO2 Data (BKG CORR)")
+xlabel("Temp (C)")
+ylabel("CO2 Absorption")
+legend(filename, filenameA, "Location","northwest")
+grid on
+hold off
+%}
+
+
+%Separate plots
+figure(1) %comparing the water peaks of the two samples
+plot(FTIR_temp_H2O,WaterData-z, "LineWidth",1,"Color","b")
+hold on
+plot(FTIR_temp_H2O_A,WaterDataA-z_A, "LineWidth",1,"Color","c")
+title("H2O Data - absorption at peak wavenumber (BKG CORR)")
 xlabel("Temp (C)")
 ylabel("H2O Absorption")
 legend(filename, filenameA, "Location","northwest")
@@ -267,22 +534,24 @@ grid on
 hold off
 
 figure(2) %comparing the CO2 peaks of the two samples
-plot(FTIR_temp,CO2Data, "LineWidth",1,"Color","#7E2F8E")
+plot(FTIR_temp_CO2,CO2Data-zz, "LineWidth",1,"Color","#7E2F8E")
 hold on
-plot(FTIR_temp_A,CO2DataA, "LineWidth",1,"Color","#FF00FF")
-title("CO2 Data - absorption at peak wavenumber")
+plot(FTIR_temp_CO2_A,CO2DataA-zz_A, "LineWidth",1,"Color","#FF00FF")
+title("CO2 Data - absorption at peak wavenumber (BKG CORR)")
 xlabel("Temp (C)")
 ylabel("CO2 Absorption")
 legend(filename, filenameA, "Location","northwest")
 grid on
 hold off
+%}
+
 
 figure(3) %Water and CO2 data of just sample 1
-plot(FTIR_temp,WaterData,"LineWidth",1,"Color","b")
+plot(FTIR_temp_H2O,WaterData-z,"LineWidth",1,"Color","b")
 hold on
-plot(FTIR_temp,CO2Data,"LineWidth",1,"Color","r")
+plot(FTIR_temp_CO2,CO2Data-zz,"LineWidth",1,"Color","r")
 title(filename)
-subtitle('H2O vs CO2')
+subtitle('H2O vs CO2 (BKG CORR)')
 xlabel("Temp (C)")
 ylabel("Absorption value")
 legend("H2O","CO2","Location","northwest")
@@ -290,11 +559,11 @@ grid on
 hold off
 
 figure(4) %Water and CO2 data of just sample 2
-plot(FTIR_temp_A,WaterDataA,"LineWidth",1,"Color","b")
+plot(FTIR_temp_H2O_A,WaterDataA-z_A,"LineWidth",1,"Color","b")
 hold on
-plot(FTIR_temp_A,CO2DataA,"LineWidth",1,"Color","r")
+plot(FTIR_temp_CO2_A,CO2DataA-zz_A,"LineWidth",1,"Color","r")
 title(filenameA)
-subtitle('H2O vs CO2')
+subtitle('H2O vs CO2 (BKG CORR)')
 xlabel("Temp (C)")
 ylabel("Absorption value")
 legend("H2O","CO2","Location","northwest")
@@ -304,7 +573,83 @@ hold off
 
 
 
-%%
+%% BACKGROUND PLOTTING
+% Each plot will only display IF their corresponding sample needed
+% background correction
+
+% Sample 1 water and CO2 background correction
+if back_corr == 0
+    disp('no back corr for sample 1')
+else
+    figure;
+    subplot(2,2,1)
+    plot(FTIR_temp_H2O,WaterData,'b',FTIR_temp_H2O,z,'r');
+    xlabel('Temp (C)')
+    ylabel('H2O abs')
+    legend('Spec','Bkg est');
+    title(filename)
+    subtitle(['Estimation with function ''' fct ''', order = ' num2str(ord) ' and threshold = ' num2str(s)])
+    subplot(2,2,3)
+    plot(FTIR_temp_H2O,WaterData-z,'r')
+    xlabel('Temp (C)')
+    ylabel('H2O abs')
+    subtitle('Corrected spectrum')
+    %legend('Estimated corrected spectrum')
+    
+    subplot(2,2,2)
+    plot(FTIR_temp_CO2,CO2Data,'b',FTIR_temp_CO2,zz,'r')
+    xlabel('Temp (C)')
+    ylabel('CO2 abs')
+    legend('Spec','Bkg est','Location','north')
+    %title(filename)
+    subtitle(['Estimation with function ''' fctfct ''', order = ' num2str(ordord) ' and threshold = ' num2str(ss)])
+    subplot(2,2,4)
+    plot(FTIR_temp_CO2,CO2Data-zz,'r')
+    xlabel('Temp (C)')
+    ylabel('CO2 abs')
+    subtitle('Corrected spectrum')
+    %legend('Estimated corrected spectrum')
+end
+
+
+
+% Sample 2 water and CO2 background plotting
+if back_corr_A == 0
+    disp('no back corr for sample 2')
+else
+    figure;
+    subplot(2,2,1)
+    plot(FTIR_temp_H2O_A,WaterDataA,'b',FTIR_temp_H2O_A,z_A,'r')
+    xlabel('Temp (C)')
+    ylabel('H2O abs')
+    legend('Spec','Bkg est')
+    title(filenameA)
+    subtitle(['Estimation with function ''' fct_A ''', order = ' num2str(ord_A) ' and threshold = ' num2str(s_A)])
+    subplot(2,2,3)
+    plot(FTIR_temp_H2O_A,WaterDataA-z_A,'r')
+    xlabel('Temp (C)')
+    ylabel('H2O abs')
+    subtitle('Corrected spectrum')
+    %legend('Estimated corrected spectrum')
+    
+    subplot(2,2,2)
+    plot(FTIR_temp_CO2_A,CO2DataA,'b',FTIR_temp_CO2_A,zz_A,'r')
+    xlabel('Temp (C)')
+    ylabel('CO2 abs')
+    legend('Spec','Bkg est','Location','north')
+    %title(filenameA)
+    subtitle(['Estimation with function ''' fctfct_A ''', order = ' num2str(ordord_A) ' and threshold = ' num2str(ss_A)])
+    subplot(2,2,4)
+    plot(FTIR_temp_CO2_A,CO2DataA-zz_A,'r')
+    xlabel('Temp (C)')
+    ylabel('CO2 abs')
+    subtitle('Corrected spectrum')
+    %legend('Estimated corrected spectrum')
+end
+
+
+
+%% EXTRA PLOTTING
 
 %{
 
